19fcc1777cbd7f0c2c9253008a5636eba814c102
Alexander Baron <alexbaron50@gmail.com>
2018-01-05 12:27:39 -0500

    Saving new images

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -6,31 +6,40 @@ use App\Image;
 use App\Thumbnail;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Storage;
 use DB;
 
 class ImageController extends Controller
 {
+	private $thumbSizes = [
+		[200,200],
+		[400,300],
+		[500,500],
+	];
+
     public function index() {
 
 	    $response = Image::where('status', 'active')
-	                ->get()
-	                ->map(function($image){
-		                $attributes = $image->attributesToArray();
+	                     ->orderBy('id', 'desc')
+	                     ->get()
+	                     ->map(function($image){
+		                     $attributes = $image->attributesToArray();
 
-		                $attributes['thumbnails'] = Thumbnail::where('image_id', $attributes['id'])
-		                                                     ->get()
-		                                                     ->map(function($thumb) {
-			                                                     return $thumb->attributesToArray();
-		                                                     });
+		                     $attributes['thumbnails'] = Thumbnail::where('image_id', $attributes['id'])
+		                                                          ->get()
+		                                                          ->map(function($thumb) {
+			                                                          return $thumb->attributesToArray();
+		                                                          });
 
-		                return $attributes;
-	                });
+		                     return $attributes;
+	                     });
 
 	    return response()->json($response);
     }
 
     public function deleted() {
 	    return Image::where('status', 'deleted')
+	                ->orderBy('id', 'desc')
 	                ->get()
 	                ->map(function($image){
 		                $attributes = $image->attributesToArray();
@@ -54,7 +63,45 @@ class ImageController extends Controller
     }
 
     public function store(Request $request) {
-    	return Image::create($request->all());
+
+	    $imageManager = new \Intervention\Image\ImageManager();
+
+	    $image = $imageManager->make($request['image_data'])->stream();
+
+	    $imageData = Image::create([
+		    'name' => $request['image_name'],
+		    'path' => $request['image_filename'],
+		    'status' => 'active'
+	    ]);
+
+	    $imageAttrs = $imageData->attributesToArray();
+
+	    Storage::put("public/images/{$imageAttrs['id']}/".$request['image_filename'], $image);
+
+	    $pathAsArray = explode(".", $imageAttrs['path']);
+	    $extension = end($pathAsArray);
+	    $baseName = basename($imageAttrs['path'], ".{$extension}");
+
+	    array_map(function($size) use ($image, $extension, $imageAttrs, $baseName, $imageManager){
+
+
+		    $thumb = $imageManager->make($image)->fit($size[0], $size[1])->stream($extension, 80);
+
+		    Storage::put("public/images/{$imageAttrs['id']}/{$baseName}-{$size[0]}x{$size[1]}.{$extension}", $thumb);
+
+		    Thumbnail::create([
+			    'path' => "{$baseName}-{$size[0]}x{$size[1]}.{$extension}",
+			    'image_id' => $imageAttrs['id'],
+			    'width' => $size[0],
+			    'height' => $size[1]
+		    ]);
+
+	    }, $this->thumbSizes);
+
+    	return response()->json([
+    		'success' => true,
+		    'data' => $imageData
+	    ]);
     }
 
     public function update(Request $request, $id) {
